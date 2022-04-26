f01988127ad1cf23ce24207256d2243deb079fcd
PARGSoft <pargsoft@hotmail.com>
2022-04-26 10:32:09 +0100

    fix dir

--- obj/Debug/net472/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace SIT.Tarkov.Core
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "SIT.Tarkov.Core";
-        public const string PLUGIN_NAME = "SIT.Tarkov.Core";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
