ea03b26101e23a598fa8e9e656d6437df7a5f47a
PARGSoft <pargsoft@hotmail.com>
2022-06-06 20:58:03 +0100

    Add Code Generator Tools by Aki

---
+++ CodeWithLabel.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Reflection.Emit;
+
+namespace SIT.Tarkov.Core
+{
+    public class CodeWithLabel : Code
+    {
+        public Label Label { get; }
+
+        public CodeWithLabel(OpCode opCode, Label label) : base(opCode)
+        {
+            Label = label;
+        }
+
+        public CodeWithLabel(OpCode opCode, Label label, object operandTarget) : base(opCode, operandTarget)
+        {
+            Label = label;
+        }
+
+        public CodeWithLabel(OpCode opCode, Label label, Type callerType) : base(opCode, callerType)
+        {
+            Label = label;
+        }
+
+        public CodeWithLabel(OpCode opCode, Label label, Type callerType, object operandTarget, Type[] parameters = null) : base(opCode, callerType, operandTarget, parameters)
+        {
+            Label = label;
+        }
+
+        public override Label? GetLabel()
+        {
+            return Label;
+        }
+    }
+}
