c624b3de0ee872e9e8f309f7cb6ab6a31d836f5c
Paulov <pargsoft@hotmail.com>
2022-06-30 14:39:43 +0100

    Tarkov v0.12.12.30 Fix - Experience Gain Fix

--- PlayerPatches/ExperienceGainFix.cs
+++ PlayerPatches/ExperienceGainFix.cs
@@ -1,4 +1,5 @@
 ﻿using SIT.Tarkov.Core;
+using System;
 using System.Linq;
 using System.Reflection;
 
@@ -12,38 +13,46 @@ namespace SIT.Tarkov.Core
         }
 
         [PatchPrefix]
-        static void PrefixPatch(ref bool isLocal)
+        static void PrefixPatch(ref bool isOnline)
         {
-            isLocal = false;
+            isOnline = false;
         }
 
         [PatchPostfix]
-        static void PostfixPatch(ref bool isLocal)
+        static void PostfixPatch(ref bool isOnline)
         {
-            isLocal = true;
+            isOnline = true;
         }
 
         protected override MethodBase GetTargetMethod()
         {
-            return typeof(EFT.UI.SessionEnd.SessionResultExperienceCount)
-                 .GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly)
-                 .FirstOrDefault(IsTargetMethod);
+            var returnedType = PatchConstants.EftTypes.Single(x =>
+                 x.FullName.StartsWith(typeof(EFT.UI.SessionEnd.SessionResultExperienceCount).FullName)
+                 //x.FullName.StartsWith("EFT.UI.SessionEnd.SessionResultExperienceCount")
+                 && PatchConstants.GetPropertyFromType(x, "KeyScreen") != null
+                 );
+            //Logger.LogInfo(returnedType.FullName);
+
+            //return PatchConstants.GetAllMethodsForType(returnedType, true).First();
+            return returnedType.GetConstructor((new Type[] { typeof(EFT.Profile), typeof(bool), typeof(EFT.ExitStatus) }));
         }
 
-        private static bool IsTargetMethod(MethodInfo mi)
-        {
-            var parameters = mi.GetParameters();
+        //private static bool IsTargetMethod(MethodInfo mi)
+        //{
+        //    var parameters = mi.GetParameters();
 
-            if (parameters.Length != 3
-                || parameters[0].Name != "profile"
-                || parameters[1].ParameterType != typeof(bool)
-                || parameters[1].Name != "isLocal"
-                || parameters[2].Name != "exitStatus")
-            {
-                return false;
-            }
+        //    return (parameters.Length >= 3
+        //        && parameters[0].Name == "profile"
+        //        && parameters[1].ParameterType == typeof(bool)
+        //        && parameters[1].Name == "isLocal"
+        //        && parameters[2].Name == "exitStatus")
+        //        ||
+        //        (parameters.Length >= 3
+        //        && parameters[0].Name == "profile"
+        //        && parameters[1].ParameterType == typeof(bool)
+        //        && parameters[1].Name == "isOnline"
+        //        && parameters[2].Name == "exitStatus");
 
-            return true;
-        }
+        //}
     }
 }
