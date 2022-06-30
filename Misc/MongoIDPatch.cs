4dbd3f55145954220deb48dbd610fb2e0df4708c
Paulov <pargsoft@hotmail.com>
2022-06-30 14:39:59 +0100

    Tarkov v0.12.12.30 Fix - Mongo ID Fix (Traders)

---
+++ Misc/MongoIDPatch.cs
@@ -0,0 +1,33 @@
+﻿using EFT;
+using SIT.Tarkov.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIT.Tarkov.Core.Misc
+{
+    internal class MongoIDPatch : ModulePatch
+    {
+        public static bool MongoIDExists = PatchConstants.EftTypes.Any(x => x.FullName == "EFT.MongoID");
+
+        protected override MethodBase GetTargetMethod()
+        {
+            return PatchConstants.EftTypes.Single(x => x.FullName == "EFT.MongoID").GetConstructor(new Type[] { typeof(string) });
+        }
+
+        [PatchPrefix]
+        public static bool PatchPrefix(ref string id)
+        {
+            Logger.LogInfo("MongoIDPatch: Id to Parse: " + id);
+            if(id.StartsWith("I-"))
+            {
+                id = new MongoID(true);
+            }
+            Logger.LogInfo("MongoIDPatch: NewId: " + id);
+            return true;
+        }
+    }
+}
