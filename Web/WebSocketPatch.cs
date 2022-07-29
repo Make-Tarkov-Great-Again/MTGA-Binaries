683a52c1e81e1ef3036f51ae7ae5f92277032c40
Paulov <pargsoft@hotmail.com>
2022-07-29 11:42:19 +0100

    NEW: Working WebSocketPatch with Address provided by Web Server

--- Web/WebSocketPatch.cs
+++ Web/WebSocketPatch.cs
@@ -1,32 +1,40 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Reflection;
 
 namespace SIT.Tarkov.Core
 {
-    
+    /// <summary>
+    /// The Web Socket patch affects the Notifications pushed from Web Server to the Client via Sockets
+    /// The client's class that handles all of this contains OnNotificationReceived, Activate
+    /// If you look at "method_7" (at the time of writing), you can see how it parses the notification via ENotificationType to the Client to process
+    /// </summary>
     public class WebSocketPatch : ModulePatch
     {
+        public static string WebSocketAddress { get; private set; }
+        public static Uri WebSocketUri { get; private set; }
+
+        /// <summary>
+        /// On creation of the WebSocketPatch Instance, Get the web server's determined WebSocket address
+        /// </summary>
+        public WebSocketPatch()
+        {
+            WebSocketAddress = new Request().PostJson("/client/WebSocketAddress", null);
+
+            if (Uri.TryCreate(WebSocketAddress, UriKind.Absolute, out Uri uri))
+            {
+                WebSocketUri = uri;
+                Logger.LogInfo("[WEBSOCKET] Address is " + WebSocketAddress);
+            }
+            else
+            {
+                Logger.LogError("[WEBSOCKET] Failed to set websocket " + WebSocketAddress);
+            }
+        }
         protected override MethodBase GetTargetMethod()
         {
-            //_ = GInterface143;
-
-            /*
-                ENotificationTransportType TransportType { get; }
-                IReadOnlyDictionary<string, string> UriParams { get; }
-                IReadOnlyDictionary<string, string> RequestHeaders { get; }
-                bool Closed { get; }
-
-                void Close();
-                void Open(Action<long, byte[]> onMessage, Action<long, string> onError);
-                void SetRequestHeader(string key, string value);
-                void SetUri(string uri);
-                void SetUriParam(string key, string value);
-             */
-            //var targetInterface = PatchConstants.EftTypes.Single(x => x == typeof(IConnectionHandler) && x.IsInterface);
-            //var typeThatMatches = PatchConstants.EftTypes.Single(x => targetInterface.IsAssignableFrom(x) && x.IsAbstract && !x.IsInterface);
-            //return typeThatMatches.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance).Single(x => x.ReturnType == typeof(Uri));
             var targetInterface = PatchConstants.TypesDictionary["EftTypes"]// PatchConstants.EftTypes
                 .Single(
                 x => 
@@ -51,32 +59,9 @@ namespace SIT.Tarkov.Core
         [PatchPrefix]
         private static bool PatchPrefix(ref Uri __result)
         {
-            if (__result != null)
-            {
-                Logger.LogInfo("WebSocketPatch:PatchPrefix:" + __result.ToString());
-            }
-            if (PatchConstants.GetBackendUrl().Contains("https"))
-            {
-                __result = new Uri(PatchConstants.GetBackendUrl().Replace("https", "wss"));
-                Logger.LogInfo("[WEBSOCKET] URL contains https, changing to wss");
-
-            } 
-            else
-            {
-                __result = new Uri(PatchConstants.GetBackendUrl().Replace("http", "ws"));
-                Logger.LogInfo("[WEBSOCKET] URL contains http, changing to ws");
-            }
-            Logger.LogInfo("[WEBSOCKET] Patched");
+            __result = WebSocketUri;
             return false;
         }
-
-        //[PatchPostfix]
-        //private static void PatchPostfix(ref Uri __result)
-        //{
-        //    Logger.LogInfo("WebSocketPatch:PatchPostfix:" + __result.ToString());
-        //    //return new Uri(__instance.ToString().Replace("wss:", "ws:"));
-        //    __result = new Uri(PatchConstants.GetBackendUrl().Replace("https", "ws"));
-        //}
     }
 
 }
