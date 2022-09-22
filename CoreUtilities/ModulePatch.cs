1c099157c271f4ca3d980ebbc820003f801d0d03
ae_King <aclengen@gmail.com>
2022-09-22 09:08:32 -0400

    Removed intrusive legality check

--- CoreUtilities/ModulePatch.cs
+++ CoreUtilities/ModulePatch.cs
@@ -33,7 +33,6 @@ namespace SIT.Tarkov.Core
         /// <param name="name">Name</param>
         protected ModulePatch(string name = null)
         {
-            A.Tarkov.Core.Plugin.LegalityCheck();
             _harmony = new Harmony(name ?? GetType().Name);
             _prefixList = GetPatchMethods(typeof(PatchPrefixAttribute));
             _postfixList = GetPatchMethods(typeof(PatchPostfixAttribute));
